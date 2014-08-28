46dc9753a6cfa2c91e047cedc4f242bdc78b8f61
Javier Baccarelli <Javier.Baccarelli@wnco.com>
2014-08-28 15:48:00 -0500

    Javier Baccarelli / Ale Arce - MUT-003 - Replacing gulp server with express and deleting unused variables and files.

--- router.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-function route (pathname) {
-
-}
-
-exports.route = route;
--- server.js
+++ server.js
@@ -1,22 +1,10 @@
 'use strict';
 
-var http = require('http'),
-    url = require('url');
+var express = require('express');
+var app = express();
+ 
+app.listen(3000);
 
-function startServer(route) {
-    function onRequest(request, response) {
-        var pathname = url.parse(request.url).pathname;
-
-        route(pathname);
-
-        console.log('request received...');
-        response.writeHead(200, {"Content-Type": "text/plain"});
-        response.write("Hello World");
-        response.end();
-    }
-
-    http.createServer(onRequest).listen(8000);
-    console.log('server has started');
-}
-
-exports.start = startServer;
+app.get('/view', function(req, res) {
+    res.sendfile('./app-components/src/index.html');
+});
