5f56ccb93310ce601b55dbf2fd2bf7230f6b2f6e
Ale Arce <ale.arce@wnco.com>
2014-08-29 13:17:54 -0500

    Adding some stuff, still get an error when 'gulp' excecuted

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,22 @@
 'use strict';
 
-var gulp = require('gulp'),
+var browserify = require('gulp-browserify'),
+    concat = require('gulp-concat'),
+    gulp = require('gulp'),
     react = require('gulp-react'),
     server = require('./server');
+
+gulp.task('copy', function() {
+    gulp.src('./app-components/src/index.html')
+        .pipe(gulp.dest('./app-components/target'));
+});
+
+gulp.task('browserify', function() {
+    gulp.src('./app-components/src/main.js')
+        .pipe(browserify({transform: 'reactify'}))
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest('./app-components/target/main.js'));
+});
 
 gulp.task('compile-react', function () {
     return gulp.src('./app-components/src/components/**/*.jsx')
@@ -10,4 +24,4 @@ gulp.task('compile-react', function () {
         .pipe(gulp.dest('./app-components/target/components'));
 });
 
-gulp.task('default', ['compile-react']);
+gulp.task('default', ['compile-react', 'browserify', 'copy']);
