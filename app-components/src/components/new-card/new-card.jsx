de2cd4634f1a03e3b450c86a72f6cd700908ebe4
Ale Arce <ale.arce@wnco.com>
2014-09-09 12:25:45 -0500

    New card Refactor

--- app-components/src/components/new-card/new-card.jsx
+++ app-components/src/components/new-card/new-card.jsx
@@ -12,43 +12,43 @@ NewCard = React.createClass ({
 
 	render: function () {
 		return (
-			<div className="new-card-container">
-				<input className="pretty-button" type="button" value="New Card" onClick={this.showNewCard}/>
+			<div className='new-card-container'>
+				<input className='pretty-button' type='button' value='New Card' onClick={this.showNewCard}/>
                 <div className='new-card hidden' id='new-card-box'>
                     <select id='usersSelect' onChange={this.getImage}>
-                        <option value="select" selected={true}>Seleccione Usuario</option>
-                        <option value="ale.arce.lr">Ale Arce</option>
-                        <option value="kavi089">Javi Baccarelli</option>
-                        <option value="nicolas.siandro">Nicolas Siandro</option>
-                        <option value="charca">Maxi Ferreira</option>
-                        <option value="sanozukecze">Gonza Miranda</option>
+                        <option value='select' selected={true}>Seleccione Usuario</option>
+                        <option value='ale.arce.lr'>Ale Arce</option>
+                        <option value='kavi089'>Javi Baccarelli</option>
+                        <option value='nicolas.siandro'>Nicolas Siandro</option>
+                        <option value='charca'>Maxi Ferreira</option>
+                        <option value='sanozukecze'>Gonza Miranda</option>
                     </select>
                     <select id='cat'>
-                        <option value="mutombo" selected={true}>Mutombo</option>
-                        <option value="mugre">Mugre</option>
-                        <option value="reunion">Reunión</option>
-                        <option value="demo">Demo Exitosa</option>
-                        <option value="ingreso">Ingreso</option>
-                        <option value="cumple">Cumpleaños</option>
-                        <option value="despedida">Despedida</option>
-                        <option value="otra">Otra...</option>
+                        <option value='mutombo' selected={true}>Mutombo</option>
+                        <option value='mugre'>Mugre</option>
+                        <option value='reunion'>Reunión</option>
+                        <option value='demo'>Demo Exitosa</option>
+                        <option value='ingreso'>Ingreso</option>
+                        <option value='cumple'>Cumpleaños</option>
+                        <option value='despedida'>Despedida</option>
+                        <option value='otra'>Otra...</option>
                     </select>
                     <img className='default-image' src={this.state.imageSrc} />
-    				<input className="button" type="button" value="Add" onClick={this.addCard}/>
+    				<input className='button' type='button' value='Add' onClick={this.addCard}/>
                 </div>
             </div>
 		)
 	},
 
     getImage: function (e) {
-        if(document.getElementById("usersSelect").selectedIndex === 0) {
+        if(document.getElementById('usersSelect').selectedIndex === 0) {
             this.setState({
                     imageSrc: 'http://notsportscenter.com/wp-content/uploads/2014/03/MutomboWag.png'
             });
         }
         else {
             this.setState({
-                    imageSrc: 'https://graph.facebook.com/' + document.getElementById("usersSelect").value + '/picture?width=150&height=150'
+                    imageSrc: 'https://graph.facebook.com/' + document.getElementById('usersSelect').value + '/picture?width=150&height=150'
             });
         }
     },
@@ -64,13 +64,13 @@ NewCard = React.createClass ({
     },
 
 	addCard: function () {
-        if (!document.getElementById("usersSelect").selectedIndex == 0) {
-    		firebaseRefs = new Firebase("https://mutombo-cards.firebaseio.com/");
+        if (!document.getElementById('usersSelect').selectedIndex == 0) {
+    		firebaseRefs = new Firebase('https://mutombo-cards.firebaseio.com/');
 
     		firebaseRefs.push({
-    			name: document.getElementById("usersSelect").options[document.getElementById("usersSelect").selectedIndex].innerHTML,
-    			user: document.getElementById("usersSelect").value,
-    			cat: document.getElementById("cat").options[document.getElementById("cat").selectedIndex].innerHTML,
+    			name: document.getElementById('usersSelect').options[document.getElementById('usersSelect').selectedIndex].innerHTML,
+    			user: document.getElementById('usersSelect').value,
+    			cat: document.getElementById('cat').options[document.getElementById('cat').selectedIndex].innerHTML,
     			date: Moment(Date.now()).format('DD/MM/YYYY, hh:mm')
     		});
             this.showNewCard();
